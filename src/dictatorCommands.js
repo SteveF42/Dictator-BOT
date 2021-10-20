c8f5ed2714694b9f3f1492025e71ac9d17c06cb3
Steve Flores <stevewflores43@gmail.com>
2021-10-20 16:52:20 -0700

    implemented JSON db instead of reading writing to file/ started implementing some commands for dictator

--- src/dictatorCommands.js
+++ src/dictatorCommands.js
@@ -4,7 +4,15 @@ const dictatorCmds = require("./dictatorUtil")
 
 
 function execCommand(cmd,message){
-    if(cmd == "deploy") dictatorCmds.deploy(message);
+    if(cmd === "deploy") dictatorCmds.deploy(message);
+
+    if (cmd === "commands") dictatorCmds.commands(message);
+
+    if (cmd === 'steve') {
+        message.channel.send('https://www.youtube.com/watch?v=v7szH6ZWKkg')
+    }
+
+    if(cmd === 'dictator') dictatorCmds.dictator(message)
 }
 
 
--- src/dictatorUtil.js
+++ src/dictatorUtil.js
@@ -13,22 +13,52 @@ const COMMANDS = [
 ]
 
 
-async function deploy(message){
+async function deploy(message) {
     const channelID = message.channelID;
     const guildID = message.guildID
 
     const json = await file.read_file()
-    
+
     json[guildID] = {
         channelID,
-        users:{}
+        users: {}
     }
-    
+
     file.write_file(json)
 }
 
-async function checkIfGuildActive(){
+function commands(message) {
+
+    const commands = `
+                !commands
+                !dictator (join the dictator pool)
+                !overthrow (all people need to vote to overthrow)
+                !get [dictator,...]
+                !remove_user (owner ONLY)
+                !rotate (owner ONLY)
+                !play (play tic tac toe)
+                !leave (leave tic tac toe)
+            `
+
+    const embed = new MessageEmbed()
+        .setTitle('All Commands')
+        .setColor('#2a80f7')
+        .setDescription(commands)
+
+    message.channel.send(embed)
+
+}
+
+function dictator(message){
+    //get the guildID
+    //get user ID
+    //if guild already has dictator position then add it to that if not push the guild onto DB
+    
     
 }
 
-module.exports = {deploy}
+async function checkIfGuildActive() {
+
+}
+
+module.exports = { deploy, commands, dictator}
